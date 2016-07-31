60d43ca0309d25e00521f13a6cc52cc87a6788ed
Angelo Senna <info@angelosenna.com>
2016-07-31 20:59:30 +1000

    Boundaries, hazards and enemies
    Boundary
    Creating hazards
    Explosions
    Game Controller

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+        Instantiate(explosion, rb.transform.position, rb.transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
